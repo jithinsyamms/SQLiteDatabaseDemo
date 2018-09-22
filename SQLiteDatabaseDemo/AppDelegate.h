0b50f86b3f96531ac6c61323775f9415aa3fcf01
jithin <jithinsyamms@gmail.com>
2018-09-22 15:51:08 +0530

    Initial Commit

---
+++ SQLiteDatabaseDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SQLiteDatabaseDemo
+//
+//  Created by Jithin on 22/09/18.
+//  Copyright © 2018 Jithin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SQLiteDatabaseDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SQLiteDatabaseDemo
+//
+//  Created by Jithin on 22/09/18.
+//  Copyright © 2018 Jithin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
